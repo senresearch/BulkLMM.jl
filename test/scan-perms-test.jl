2918d26ffcf1195d36677216b2c0131359b806b9
Zifan Yu <zfredyu@uw.edu>
2022-08-25 11:06:31 -0500

    Added a function that excludes the complementary columns in geno probs data.

---
+++ test/scan-perms-test.jl
@@ -0,0 +1,37 @@
+# Scan-perms Tests - Tests for genome scan of univariate trait and its permutated copies
+
+## Loading required libraries
+using DelimitedFiles
+using LinearAlgebra
+using Optim
+using Distributions
+using Distributed
+using Test
+using BenchmarkTools
+using CSV
+using DataFrames
+using Random
+
+## Loading functions to be tested:
+include("../src/scan.jl")
+include("../src/kinship.jl")
+include("../src/lmm.jl")
+include("../src/util.jl")
+include("../src/wls.jl")
+include("../src/readData.jl");
+
+##########################################################################################################
+## Read-in Data (BXD data)
+##########################################################################################################
+pheno_file = "../data/bxdData/traits.csv"
+pheno = readBXDpheno(pheno_file);
+geno_file = "../data/bxdData/geno_prob.csv"
+geno = readGenoProb2(geno_file);
+kinship = CSV.File("../../data/BXDkinship.csv") |> DataFrame |> Matrix;
+
+
+
+
+##########################################################################################################
+## TEST: resid()
+##########################################################################################################
