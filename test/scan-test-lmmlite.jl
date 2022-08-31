b208fd2459e5cc62bb2f516deac7b68898a97dcd
Zifan Yu <zfredyu@uw.edu>
2022-08-31 10:52:49 -0500

    Tested basic scan_null results with lmmlite results.

---
+++ test/scan-test-lmmlite.jl
@@ -0,0 +1,60 @@
+# This test file will test univariate scan functions by running on BXD data and comparing results with R lmmlite package
+
+## Load required packages:
+using DelimitedFiles
+using LinearAlgebra
+using Optim
+using Distributions
+using Random
+using CSV
+using DataFrames
+### For writing tests:
+using Test
+using BenchmarkTools
+
+## Include the source code of BulkLMM to be tested:
+include("../src/scan.jl");
+include("../src/lmm.jl");
+include("../src/wls.jl");
+include("../src/util.jl");
+include("../src/readData.jl");
+
+## Also include the helper functions for writing tests:
+include("testHelper.jl");
+
+## Read in BXD data:
+pheno_file = "../data/bxdData/BXDtraits.csv"
+pheno = readBXDpheno(pheno_file);
+geno_file = "../data/bxdData/BXDgeno_prob.csv"
+geno = readGenoProb_ExcludeComplements(geno_file);
+
+kinship = calcKinship(geno); # calculate kinship matrix from genotype data
+
+## Consider the 7919-th trait
+pheno_y = reshape(pheno[:, 7919], :, 1);
+
+## Run BulkLMM code to get results:
+reml_results = scan(pheno_y, geno, kinship; reml = true); # by default uses scan_null;
+ml_results = scan(pheno_y, geno, kinship; reml = false);
+
+lods_BulkLMM_reml = reshape(reml_results[3], :, 1);
+lods_BulkLMM_ml = reshape(ml_results[3], :, 1);
+
+
+## Compare with lmmlite results:
+## Read in lmmlite results:
+reml_results_lmmlite = CSV.read("notebooks/lmmlite_R/output/result.lmmlite_REML.csv", DataFrame);
+ml_results_lmmlite = CSV.read("notebooks/lmmlite_R/output/result.lmmlite_ML.csv", DataFrame);
+
+lods_lmmlite_reml = parse.(Float64, reshape(reml_results_lmmlite[2:end, 5], :, 1));
+lods_lmmlite_ml = parse.(Float64, reshape(ml_results_lmmlite[2:end, 5], :, 1));
+
+## Testings:
+
+@testset "lmmlite_results_tests" begin
+    tol = 1e-9;
+    @test sumSqDiff(lods_lmmlite_reml, lods_BulkLMM_reml) <= sqrt(tol);
+    @test sumSqDiff(lods_lmmlite_ml, lods_BulkLMM_ml) <= sqrt(tol);
+    @test maxSqDiff(lods_lmmlite_reml, lods_BulkLMM_reml) <= tol;
+    @test maxSqDiff(lods_lmmlite_ml, lods_BulkLMM_ml) <= tol;
+end
--- test/testHelper.jl
+++ test/testHelper.jl
@@ -1,3 +1,16 @@
 function testHelper(test::Expr; ntests::Integer = 10)
     [eval(test) for i in 1:ntests]
 end
+
+## Helper functions for comparing results:
+function maxSqDiff(a::Array{Float64, 2}, b::Array{Float64, 2})
+
+    return maximum((a .- b) .^2)
+
+end
+
+function sumSqDiff(a::Array{Float64, 2}, b::Array{Float64, 2})
+
+    return sum((a .- b) .^2)
+
+end
