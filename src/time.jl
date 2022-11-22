cf32bc58bcbd0c1002dfc97ace74988b5a966485
Zifan Yu <zfredyu@uw.edu>
2022-11-22 10:46:05 -0600

    Revised the function for evaluating runtime performance.

--- src/time.jl
+++ src/time.jl
@@ -1,23 +1,23 @@
 
-function runtime(funlist,n)
+function runtime(funlist::Array{String, 1}, n::Int64)
     # number of functions to be compared
     nf = length(funlist)
 
-    elapsedTimes = Array(Float64,(n,nf))
-    medianTimes = Array(Float64,nf)
+    elapsedTimes = Array{Float64, 2}(undef, n, nf);
+    medianTimes = Array{Float64, 1}(undef, nf);
     idx = 1
 
-    for i=1:n
-        for j=1:nf
+    for i = 1:n # do n times running each function
+        for j = 1:nf # run each function
             t0 =time()
-            eval(parse(funlist[j]))
+            eval(funlist[j])
             t1 = time()
-            elapsedTimes[i,j] = t1-t0
+            elapsedTimes[i, j] = t1-t0
         end
     end
 
     for j in 1:nf
-        medianTimes[j] = median(elapsedTimes[:,j])
+        medianTimes[j] = median(elapsedTimes[:, j])
     end
 
     return elapsedTimes, medianTimes
