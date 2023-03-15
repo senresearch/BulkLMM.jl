651e1a4ca94de30a0340671ef79c3289c9c4e128
learningMalanya <zfredyu@uw.edu>
2023-03-15 17:03:32 -0500

    Changed the check of positivity condition of weights to return warning rather than an errror.

--- src/wls.jl
+++ src/wls.jl
@@ -33,7 +33,7 @@ function wls(y::Array{Float64, 2}, X::Array{Float64, 2}, w::Array{Float64, 1}, p
 
     # check if weights are positive
     if(any(w .<= .0))
-        error("Some weights are not positive.")
+        @warn "Some weights are not positive."
     end
 
     # square root of the weights
