e4fefed647115e2bfc399ed1fae2f12d770c4b74
learningMalanya <zfredyu@uw.edu>
2023-06-11 19:01:42 -0500

    Updated new kinship function.

--- src/kinship.jl
+++ src/kinship.jl
@@ -5,25 +5,10 @@ function calcKinship(geno::Array{Float64, 2})
 
     (n, p) = size(geno); # n - the sample size; p - the number of tested markers
 
-    K = ones(n, n);
-
-    for i in 1:(n-1)
-        for j in (i+1):n
-
-            K[i, j] = K[j, i] = calcCorr_IBD(vec(geno[i, :]), vec(geno[j, :]));
-
-        end
-    end
+    X = geno .- 0.5;
+    K = 2 .* (X*X')./size(X, 2) .+ 0.5;
+    K[diagind(K)] .= 1.0
 
     return K;
-
-end
-
-function calcCorr_IBD(vg_i::Array{Float64, 1}, vg_j::Array{Float64, 1})
-
-    x_i = 0.5 .- vg_i;
-    x_j = 0.5 .- vg_j;
-
-    return mean(0.5 .+ 2 .* (x_i .* x_j))
 
 end
--- test/kinship_test.jl
+++ test/kinship_test.jl
@@ -1,5 +1,5 @@
 ## Tests for function to construct kinship:
 
 
-kinship_ref = Helium.readhe(joinpath(@__DIR__, "ref_data_for_tests", "K_ref.he");)
+kinship_ref = Helium.readhe(joinpath(@__DIR__, "ref_data_for_tests", "kinship_ref.he");)
 println("Kinship test: ", @test kinship == kinship_ref);
