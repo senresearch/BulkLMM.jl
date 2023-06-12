c97e6fe62d96c5fedb1571644748f406ad1b2d73
learningMalanya <zfredyu@uw.edu>
2023-06-11 19:27:17 -0500

    Updated new kinship function.

--- test/kinship_test.jl
+++ test/kinship_test.jl
@@ -3,4 +3,4 @@
 
 # kinship_ref = Helium.readhe(joinpath(@__DIR__, "ref_data_for_tests", "kinship_ref.he"))
 kinship_test = Helium.readhe(joinpath(@__DIR__, "ref_data_for_tests", "kinship_test.he"))
-println("Kinship test: ", @test kinship == kinship_ref);
+println("Kinship test: ", @test kinship == kinship_test);
