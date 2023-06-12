1fbd86e58149e8be0fe3e76907bfc89d69609e17
learningMalanya <zfredyu@uw.edu>
2023-06-11 20:39:25 -0500

    Updated test for kinship.jl

--- test/kinship_test.jl
+++ test/kinship_test.jl
@@ -3,5 +3,6 @@
 
 ## kinship_ref created from calling calcKinship from Julia on Beale
 kinship_ref = Helium.readhe(joinpath(@__DIR__, "ref_data_for_tests", "kinship_ref.he"))
-println("Max abs diff: ", maximum(abs.(kinship .- kinship_ref)))
-println("Kinship test: ", @test kinship == kinship_ref)
+kinship_test = Helium.readhe(joinpath(@__DIR__, "ref_data_for_tests", "kinship_test.he"))
+# println("Max abs diff: ", maximum(abs.(kinship_test .- kinship_ref)))
+println("Kinship test: ", @test kinship_test == kinship_ref)
